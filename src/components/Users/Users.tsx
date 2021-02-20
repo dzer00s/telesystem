072ce9ed36e4281929f035c4ecea924ebdb64c29
dzer00s <amakadze@yandex.ru>
2021-02-20 23:01:58 +0300

    upd Users component & add hook useActions

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,14 +1,50 @@
+import { Card, CardMedia, CardContent, Container, Grid, Typography } from '@material-ui/core';
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { fetchUsers } from '../../store/action-creators/users'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import userPhoto from '../../assets/images/icon_user.png';
+
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+      marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+    card: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+    },
+    cardMedia: {
+      paddingTop: "100%",
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));
 
 const Users: React.FC = () => {
+    const classes = useStyles();
     const {users, error, loading} = useTypedSelector(state => state.users)
-    const dispatch = useDispatch()
+    const {fetchUsers} = useActions()
 
     useEffect(() => {
-        dispatch(fetchUsers())
+        fetchUsers()
     }, [])
     if (loading) {
         return <h1>loading ...</h1>
@@ -18,11 +54,48 @@ const Users: React.FC = () => {
     }
 
     return (
-        <div>
-            {users.map(user => 
-                <div>{user.name}</div>
-            )}
+        <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Пользователи
+            </Typography>
+          </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth="lg">
+          <Grid container spacing={4}>
+            {users.map((u) => (
+              <Grid item key={u} xs={12} sm={2} md={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={userPhoto}
+                    title=""
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography align="center" gutterBottom variant="h5" component="h2">
+                      {u.name}
+                    </Typography>
+                    <Typography>{u.username}</Typography>
+                    <Typography>email: {u.email}</Typography>
+                    <hr></hr>
+                    <Typography>Адрес проживания:</Typography>
+                    <Typography>Город: {u.address.city}</Typography>
+                    <Typography>Улица: {u.address.street}</Typography>
+  
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
     );
 };
 
